b6114b983a91faf000bb90107f3f53be5a386d1d
ycrao573 <raoyuchenom@gmail.com>
2021-01-07 18:32:49 +0800

    notification

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -14,6 +14,7 @@ import About from './AboutComponent';
 import Contact from './ContactComponent';
 import Reservation from './ReservationComponent';
 import Favorites from './FavoriteComponent';
+import Login from './LoginComponent';
 
 import { DISHES } from '../shared/dishes';
 import { connect } from 'react-redux';
@@ -29,282 +30,318 @@ const mapStateToProps = state => {
 }
 
 const mapDispatchToProps = dispatch => ({
-    fetchDishes: () => dispatch(fetchDishes()),
-    fetchComments: () => dispatch(fetchComments()),
-    fetchPromos: () => dispatch(fetchPromos()),
-    fetchLeaders: () => dispatch(fetchLeaders()),
-  })
+  fetchDishes: () => dispatch(fetchDishes()),
+  fetchComments: () => dispatch(fetchComments()),
+  fetchPromos: () => dispatch(fetchPromos()),
+  fetchLeaders: () => dispatch(fetchLeaders()),
+})
 
 const ContactNavigator = createStackNavigator({
-    Contact: { screen: Contact }
+  Contact: { screen: Contact }
 }, {
-    defaultNavigationOptions: ({ navigation }) => ({
-        headerStyle: {
-            backgroundColor: "#512DA8"
-        },
-        headerTitleStyle: {
-            color: "#fff"
-        },
-        headerTintColor: "#fff",
-        headerLeft: <Icon name='menu' size={24} color='white'
-        onPress={() => navigation.toggleDrawer()}
-        />
-    })
+  defaultNavigationOptions: ({ navigation }) => ({
+    headerStyle: {
+      backgroundColor: "#512DA8"
+    },
+    headerTitleStyle: {
+      color: "#fff"
+    },
+    headerTintColor: "#fff",
+    headerLeft: <Icon name='menu' size={24} color='white'
+      onPress={() => navigation.toggleDrawer()}
+    />
+  })
 });
 
 const AboutNavigator = createStackNavigator({
-    About: { screen: About }
+  About: { screen: About }
 }, {
-    defaultNavigationOptions: ({ navigation }) => ({
-        headerStyle: {
-            backgroundColor: "#512DA8"
-        },
-        headerTitleStyle: {
-            color: "#fff"
-        },
-        headerTintColor: "#fff",
-        headerLeft: <Icon name='menu' size={24} color='white'
-        onPress={() => navigation.toggleDrawer()}
-        />
-    })
+  defaultNavigationOptions: ({ navigation }) => ({
+    headerStyle: {
+      backgroundColor: "#512DA8"
+    },
+    headerTitleStyle: {
+      color: "#fff"
+    },
+    headerTintColor: "#fff",
+    headerLeft: <Icon name='menu' size={24} color='white'
+      onPress={() => navigation.toggleDrawer()}
+    />
+  })
 });
 
 const MenuNavigator = createStackNavigator({
-    Menu: {
-        screen: Menu,
-        navigationOptions: ({ navigation }) => ({
-            headerLeft: <Icon name='menu' size={24} color='white'
-            onPress={() => navigation.toggleDrawer()}
-            />
-        }) },
-    Dishdetail: { screen: Dishdetail }
+  Menu: {
+    screen: Menu,
+    navigationOptions: ({ navigation }) => ({
+      headerLeft: <Icon name='menu' size={24} color='white'
+        onPress={() => navigation.toggleDrawer()}
+      />
+    })
+  },
+  Dishdetail: { screen: Dishdetail }
 },
-    {
-        initialRouteName: 'Menu',
-        defaultNavigationOptions: {
-            headerStyle: {
-                backgroundColor: "#512DA8"
-            },
-            headerTintColor: '#fff',
-            headerTitleStyle: {
-                color: "#fff"
-            }
-        }
+  {
+    initialRouteName: 'Menu',
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: "#512DA8"
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        color: "#fff"
+      }
     }
+  }
 );
 
 const HomeNavigator = createStackNavigator({
-    Home: { screen: Home }
+  Home: { screen: Home }
 }, {
-    defaultNavigationOptions: ({ navigation }) => ({
-        headerStyle: {
-            backgroundColor: "#512DA8"
-        },
-        headerTitleStyle: {
-            color: "#fff"
-        },
-        headerTintColor: "#fff",
-        headerLeft: <Icon name='menu' size={24} color='white'
-        onPress={() => navigation.toggleDrawer()}
-        />
-    })
+  defaultNavigationOptions: ({ navigation }) => ({
+    headerStyle: {
+      backgroundColor: "#512DA8"
+    },
+    headerTitleStyle: {
+      color: "#fff"
+    },
+    headerTintColor: "#fff",
+    headerLeft: <Icon name='menu' size={24} color='white'
+      onPress={() => navigation.toggleDrawer()}
+    />
+  })
 });
 
 const ReservationNavigator = createStackNavigator({
-    Reservation: { screen: Reservation }
-  }, {
-    defaultNavigationOptions: ({ navigation }) => ({
-        headerStyle: {
-            backgroundColor: "#512DA8"
-        },
-        headerTitleStyle: {
-            color: "#fff"
-        },
-        headerTintColor: "#fff",
-      headerLeft: <Icon name="menu" size={24}
-        iconStyle={{ color: 'white' }} 
-        onPress={() => navigation.toggleDrawer()} />    
-    })
+  Reservation: { screen: Reservation }
+}, {
+  defaultNavigationOptions: ({ navigation }) => ({
+    headerStyle: {
+      backgroundColor: "#512DA8"
+    },
+    headerTitleStyle: {
+      color: "#fff"
+    },
+    headerTintColor: "#fff",
+    headerLeft: <Icon name="menu" size={24}
+      iconStyle={{ color: 'white' }}
+      onPress={() => navigation.toggleDrawer()} />
   })
+})
 
-  const FavoritesNavigator = createStackNavigator({
-    Favorites: { screen: Favorites }
-  }, {
-    defaultNavigationOptions: ({ navigation }) => ({
-      headerStyle: {
-          backgroundColor: "#512DA8"
-      },
-      headerTitleStyle: {
-          color: "#fff"            
-      },
-      headerTintColor: "#fff",
-      headerLeft: <Icon name="menu" size={24}
-        iconStyle={{ color: 'white' }} 
-        onPress={() => navigation.toggleDrawer()} />    
-    })
+const FavoritesNavigator = createStackNavigator({
+  Favorites: { screen: Favorites }
+}, {
+  defaultNavigationOptions: ({ navigation }) => ({
+    headerStyle: {
+      backgroundColor: "#512DA8"
+    },
+    headerTitleStyle: {
+      color: "#fff"
+    },
+    headerTintColor: "#fff",
+    headerLeft: <Icon name="menu" size={24}
+      iconStyle={{ color: 'white' }}
+      onPress={() => navigation.toggleDrawer()} />
   })
-  
+})
+
+const LoginNavigator = createStackNavigator({
+  Login: { screen: Login }
+}, {
+  defaultNavigationOptions: ({ navigation }) => ({
+    headerStyle: {
+      backgroundColor: "#512DA8"
+    },
+    headerTitleStyle: {
+      color: "#fff"
+    },
+    headerTintColor: "#fff",
+    headerLeft: <Icon name="menu" size={24}
+      iconStyle={{ color: 'white' }}
+      onPress={() => navigation.toggleDrawer()} />
+  })
+});
 const CustomDrawerContentComponent = (props) => (
-    <ScrollView>
-        <View style={styles.drawerHeader}>
-          <View style={{flex:1}}>
-          <Image source={require('./images/logo.png')} style={styles.drawerImage} />
-          </View>
-          <View style={{flex: 2}}>
-            <Text style={styles.drawerHeaderText}>Ristorante Con Fusion</Text>
-          </View>
-        </View>
-        <DrawerItems {...props} />
-    </ScrollView>
-  );
+  <ScrollView>
+    <View style={styles.drawerHeader}>
+      <View style={{ flex: 1 }}>
+        <Image source={require('./images/logo.png')} style={styles.drawerImage} />
+      </View>
+      <View style={{ flex: 2 }}>
+        <Text style={styles.drawerHeaderText}>Ristorante Con Fusion</Text>
+      </View>
+    </View>
+    <DrawerItems {...props} />
+  </ScrollView>
+);
 
 const MainNavigator = createDrawerNavigator({
-    Home:
-    {
-        screen: HomeNavigator,
-        navigationOptions: {
-            title: 'Home',
-            drawerLabel: 'Home',
-            drawerIcon: ({ tintColor, focused}) => (
-              <Icon
-                name='home'
-                type='font-awesome'            
-                size={24}
-                color={tintColor}
-              />
-            ),
-        }
+  Login: 
+  { screen: LoginNavigator,
+    navigationOptions: {
+      title: 'Login',
+      drawerLabel: 'Login',
+      drawerIcon: ({ tintColor, focused }) => (
+        <Icon
+          name='sign-in'
+          type='font-awesome'            
+          size={24}
+          iconStyle={{ color: tintColor }}
+        />
+      ),
+    }
+  },
+  Home:
+  {
+    screen: HomeNavigator,
+    navigationOptions: {
+      title: 'Home',
+      drawerLabel: 'Home',
+      drawerIcon: ({ tintColor, focused }) => (
+        <Icon
+          name='home'
+          type='font-awesome'
+          size={24}
+          color={tintColor}
+        />
+      ),
+    }
+  },
+  Menu:
+  {
+    screen: MenuNavigator,
+    navigationOptions: {
+      title: 'Menu',
+      drawerLabel: 'Menu',
+      drawerIcon: ({ tintColor, focused }) => (
+        <Icon
+          name='list'
+          type='font-awesome'
+          size={22}
+          color={tintColor}
+        />
+      ),
     },
-    Menu:
-    {
-        screen: MenuNavigator,
-        navigationOptions: {
-            title: 'Menu',
-            drawerLabel: 'Menu',
-            drawerIcon: ({ tintColor, focused}) => (
-                <Icon
-                  name='list'
-                  type='font-awesome'            
-                  size={22}
-                  color={tintColor}
-                />
-              ),
-        },
-    },
-    Contact:
-    {
-        screen: ContactNavigator,
-        navigationOptions: {
-            title: 'Contact Us',
-            drawerLabel: 'Contact Us',
-            drawerIcon: ({ tintColor, focused}) => (
-                <Icon
-                  name='address-card'
-                  type='font-awesome'            
-                  size={22}
-                  color={tintColor}
-                />
-              ),
-        }
-    },
-    About:
-    {
-        screen: AboutNavigator,
-        navigationOptions: {
-            title: 'About Us',
-            drawerLabel: 'About Us',
-            drawerIcon: ({ tintColor, focused}) => (
-              <Icon
-                name='info-circle'
-                type='font-awesome'            
-                size={24}
-                color={tintColor}
-              />
-            ),
-        }
-    },
-    Reservation:
-    { screen: ReservationNavigator,
-      navigationOptions: {
-        title: 'Reserve Table',
-        drawerLabel: 'Reserve Table',
-        drawerIcon: ({ tintColor, focused }) => (
-          <Icon
-            name='cutlery'
-            type='font-awesome'            
-            size={24}
-            iconStyle={{ color: tintColor }}
-          />
-        ),
-      }
-    },
-    Favorites:
-    { screen: FavoritesNavigator,
-      navigationOptions: {
-        title: 'My Favorites',
-        drawerLabel: 'My Favorites',
-        drawerIcon: ({ tintColor, focused }) => (
-          <Icon
-            name='heart'
-            type='font-awesome'            
-            size={24}
-            iconStyle={{ color: tintColor }}
-          />
-        ),
-      }
-    },
+  },
+  Contact:
+  {
+    screen: ContactNavigator,
+    navigationOptions: {
+      title: 'Contact Us',
+      drawerLabel: 'Contact Us',
+      drawerIcon: ({ tintColor, focused }) => (
+        <Icon
+          name='address-card'
+          type='font-awesome'
+          size={22}
+          color={tintColor}
+        />
+      ),
+    }
+  },
+  About:
+  {
+    screen: AboutNavigator,
+    navigationOptions: {
+      title: 'About Us',
+      drawerLabel: 'About Us',
+      drawerIcon: ({ tintColor, focused }) => (
+        <Icon
+          name='info-circle'
+          type='font-awesome'
+          size={24}
+          color={tintColor}
+        />
+      ),
+    }
+  },
+  Reservation:
+  {
+    screen: ReservationNavigator,
+    navigationOptions: {
+      title: 'Reserve Table',
+      drawerLabel: 'Reserve Table',
+      drawerIcon: ({ tintColor, focused }) => (
+        <Icon
+          name='cutlery'
+          type='font-awesome'
+          size={24}
+          iconStyle={{ color: tintColor }}
+        />
+      ),
+    }
+  },
+  Favorites:
+  {
+    screen: FavoritesNavigator,
+    navigationOptions: {
+      title: 'My Favorites',
+      drawerLabel: 'My Favorites',
+      drawerIcon: ({ tintColor, focused }) => (
+        <Icon
+          name='heart'
+          type='font-awesome'
+          size={24}
+          iconStyle={{ color: tintColor }}
+        />
+      ),
+    }
+  },
+
 }, {
-    drawerBackgroundColor: '#D1C4E9',
-    contentComponent: CustomDrawerContentComponent
+  initialRouteName: 'Home',
+  drawerBackgroundColor: '#D1C4E9',
+  contentComponent: CustomDrawerContentComponent
 });
 
 const MainNavigatorContainer = createAppContainer(MainNavigator);
 
 class Main extends Component {
 
-    componentDidMount() {
-        this.props.fetchDishes();
-        this.props.fetchComments();
-        this.props.fetchPromos();
-        this.props.fetchLeaders();
-      }
+  componentDidMount() {
+    this.props.fetchDishes();
+    this.props.fetchComments();
+    this.props.fetchPromos();
+    this.props.fetchLeaders();
+  }
 
-    render() {
-        return (
-            <View
-                style={{
-                    flex: 1,
-                    paddingTop:
-                        Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight,
-                }}>
-                <MainNavigatorContainer />
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          paddingTop:
+            Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight,
+        }}>
+        <MainNavigatorContainer />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-    },
-    drawerHeader: {
-      backgroundColor: '#512DA8',
-      height: 140,
-      alignItems: 'center',
-      justifyContent: 'center',
-      flex: 1,
-      flexDirection: 'row'
-    },
-    drawerHeaderText: {
-      color: 'white',
-      fontSize: 24,
-      fontWeight: 'bold'
-    },
-    drawerImage: {
-      margin: 10,
-      width: 80,
-      height: 60
-    }
-  });
+  container: {
+    flex: 1,
+  },
+  drawerHeader: {
+    backgroundColor: '#512DA8',
+    height: 140,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    flexDirection: 'row'
+  },
+  drawerHeaderText: {
+    color: 'white',
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  drawerImage: {
+    margin: 10,
+    width: 80,
+    height: 60
+  }
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Main);
--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -3,6 +3,9 @@ import { Text, View, ScrollView, StyleSheet, Picker, Alert, Switch, Button, Moda
 import { Icon, Card } from 'react-native-elements';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import Moment from 'moment';
+import * as Animatable from 'react-native-animatable';
+import { Notifications } from 'expo';
+import * as Permissions from 'expo-permissions';
 
 class Reservation extends Component {
 
@@ -34,7 +37,12 @@ class Reservation extends Component {
             'Number of Guests: ' + this.state.guests + '\nSmoking? ' + this.state.smoking + '\nDate and Time:' + this.state.date,
             [
                 { text: 'Cancel', onPress: () => { console.log('Cancel Pressed'); this.resetForm(); }, style: 'cancel' },
-                { text: 'OK', onPress: () => { this.resetForm(); } },
+                {
+                    text: 'OK', onPress: () => {
+                        this.presentLocalNotification(this.state.date);
+                        this.resetForm();
+                    }
+                },
             ],
             { cancelable: false }
         );
@@ -52,80 +60,107 @@ class Reservation extends Component {
         });
     }
 
+    async obtainNotificationPermission() {
+        let permission = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+        if (permission.status !== 'granted') {
+            permission = await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+            if (permission.status !== 'granted') {
+                Alert.alert('Permission not granted to show notifications');
+            }
+        }
+        return permission;
+    }
+
+    async presentLocalNotification(date) {
+        await this.obtainNotificationPermission();
+        Notifications.presentLocalNotificationAsync({
+            title: 'Your Reservation',
+            body: 'Reservation for ' + date + ' requested',
+            ios: {
+                sound: true
+            },
+            android: {
+                sound: true,
+                vibrate: true,
+                color: '#512DA8'
+            }
+        });
+    }
+
     render() {
         return (
             <ScrollView>
                 <Animatable.View animation="zoomIn" duration={1000} delay={500}>
-                <View style={styles.formRow}>
-                    <Text style={styles.formLabel}>Number of Guests</Text>
-                    <Picker
-                        style={styles.formItem}
-                        selectedValue={this.state.guests}
-                        onValueChange={(itemValue, itemIndex) => this.setState({ guests: itemValue })}>
-                        <Picker.Item label="1" value="1" />
-                        <Picker.Item label="2" value="2" />
-                        <Picker.Item label="3" value="3" />
-                        <Picker.Item label="4" value="4" />
-                        <Picker.Item label="5" value="5" />
-                        <Picker.Item label="6" value="6" />
-                    </Picker>
-                </View>
-                <View style={styles.formRow}>
-                    <Text style={styles.formLabel}>Smoking/Non-Smoking?</Text>
-                    <Switch
-                        style={styles.formItem}
-                        value={this.state.smoking}
-                        onTintColor='#512DA8'
-                        onValueChange={(value) => this.setState({ smoking: value })}>
-                    </Switch>
-                </View>
-                <View style={styles.formRow}>
-                    <Text style={styles.formLabel}>Date and Time</Text>
-                    <TouchableOpacity style={styles.formItem}
-                        style={{
-                            padding: 7,
-                            borderColor: '#512DA8',
-                            borderWidth: 2,
-                            flexDirection: "row"
-                        }}
-                        onPress={() => this.setState({ show: true, mode: 'date', showModal: false, })}
-                    >
-                        <Icon type='font-awesome' name='calendar' color='#512DA8' />
-                        <Text >
-                            {' ' + Moment(this.state.date).format('DD-MMM-YYYY h:mm A')}
-                        </Text>
-                    </TouchableOpacity>
-                    {/* Date Time Picker */}
-                    {this.state.show && (
-                        <DateTimePicker
-                            value={this.state.date}
-                            mode={this.state.mode}
-                            minimumDate={new Date()}
-                            minuteInterval={30}
-                            onChange={(event, date) => {
-                                if (date === undefined) {
-                                    this.setState({ show: false });
-                                }
-                                else {
-                                    this.setState({
-                                        show: this.state.mode === "time" ? false : true,
-                                        mode: "time",
-                                        date: new Date(date),
-                                        showModal: false,
-                                    });
-                                }
+                    <View style={styles.formRow}>
+                        <Text style={styles.formLabel}>Number of Guests</Text>
+                        <Picker
+                            style={styles.formItem}
+                            selectedValue={this.state.guests}
+                            onValueChange={(itemValue, itemIndex) => this.setState({ guests: itemValue })}>
+                            <Picker.Item label="1" value="1" />
+                            <Picker.Item label="2" value="2" />
+                            <Picker.Item label="3" value="3" />
+                            <Picker.Item label="4" value="4" />
+                            <Picker.Item label="5" value="5" />
+                            <Picker.Item label="6" value="6" />
+                        </Picker>
+                    </View>
+                    <View style={styles.formRow}>
+                        <Text style={styles.formLabel}>Smoking/Non-Smoking?</Text>
+                        <Switch
+                            style={styles.formItem}
+                            value={this.state.smoking}
+                            onTintColor='#512DA8'
+                            onValueChange={(value) => this.setState({ smoking: value })}>
+                        </Switch>
+                    </View>
+                    <View style={styles.formRow}>
+                        <Text style={styles.formLabel}>Date and Time</Text>
+                        <TouchableOpacity style={styles.formItem}
+                            style={{
+                                padding: 7,
+                                borderColor: '#512DA8',
+                                borderWidth: 2,
+                                flexDirection: "row"
                             }}
+                            onPress={() => this.setState({ show: true, mode: 'date', showModal: false, })}
+                        >
+                            <Icon type='font-awesome' name='calendar' color='#512DA8' />
+                            <Text >
+                                {' ' + Moment(this.state.date).format('DD-MMM-YYYY h:mm A')}
+                            </Text>
+                        </TouchableOpacity>
+                        {/* Date Time Picker */}
+                        {this.state.show && (
+                            <DateTimePicker
+                                value={this.state.date}
+                                mode={this.state.mode}
+                                minimumDate={new Date()}
+                                minuteInterval={30}
+                                onChange={(event, date) => {
+                                    if (date === undefined) {
+                                        this.setState({ show: false });
+                                    }
+                                    else {
+                                        this.setState({
+                                            show: this.state.mode === "time" ? false : true,
+                                            mode: "time",
+                                            date: new Date(date),
+                                            showModal: false,
+                                        });
+                                    }
+                                }}
+                            />
+                        )}
+                    </View>
+                    <View style={styles.formRow}>
+                        <Button
+                            onPress={() => this.handleReservation()}
+                            title="Reserve"
+                            color="#512DA8"
+                            accessibilityLabel="Learn more about this purple button"
                         />
-                    )}
-                </View>
-                <View style={styles.formRow}>
-                    <Button
-                        onPress={() => this.handleReservation()}
-                        title="Reserve"
-                        color="#512DA8"
-                        accessibilityLabel="Learn more about this purple button"
-                    />
-                </View>
+                    </View>
                 </Animatable.View>
             </ScrollView>
         );
